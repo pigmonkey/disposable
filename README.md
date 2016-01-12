16960e1c889117d5e60460e825f5a832abe91f1f
Pig Monkey <pm@pig-monkey.com>
2016-01-11 18:56:35 -0800

    do not enable network namespace by default

--- README.md
+++ README.md
@@ -4,8 +4,7 @@ Disposable is a shell script used to launch a disposable application via
 [Firejail](https://github.com/netblue30/firejail).
 
 The application will be launched within a Firejail sandbox using a private home
-directory on a temporary filesystem and a new network namespace with a
-restrictive netfilter.
+directory on a temporary filesystem.
 
 This may be useful for a number of applications, but was created specifically
 with Chromium in mind. To open a shady site within an isolated and disposable
@@ -21,10 +20,16 @@ prevent the first run greeting and disable the default browser check.
 
 ### Network
 
-By default, [NetworkManager](https://wiki.gnome.org/Projects/NetworkManager)
-will be used to determine the first connected network interface. This interface
-will be used to create the new network namespace. A specific interface may be
-requested via the `-i` option.
+Optionally, the sandbox may be launched with a new network namespace and a
+restrictive netfilter by using the `-n` option.
+
+    $ disposable -n
+
+Unless otherwise specified,
+[NetworkManager](https://wiki.gnome.org/Projects/NetworkManager) will be used
+to determine the first connected network interface. This interface will be used
+to create the new network namespace. A specific interface may be requested via
+the `-i` option (which implies `-n`).
 
     $ disposable -i eth0
 
--- disposable.sh
+++ disposable.sh
@@ -8,19 +8,23 @@ usage() {
     echo "Usage: disposable.sh [OPTION...]
 Launch a disposable application via Firejail.
 
-For further isolation, the sandbox will be launched with a new network
+For further isolation, the sandbox may be launched with a new network
 namespace and a restrictive netfilter. Unless otherwise specified, the
 first connected interface (as reported by NetworkManager) will be used.
 
 Options:
-    -i      specify the ethernet interface
+    -n      create a new network namespace
+    -i      specify an ethernet interface for the network (implies -n)
     -d      create a new /dev directory"
 }
 
 process_opts() {
-    # If an interface was found or specified, activate the network namespace and
-    # filter.
-    if [[ -n "$interface" ]]; then
+    # If requested, activate the network namespace and filter.
+    if [[ -n "$network" ]]; then
+        # If an interface wasn't specified, get the first connected device.
+        if [[ -z "$interface" ]]; then
+            interface=`nmcli d | grep -m 1 connected | cut -d ' ' -f 1`
+        fi
         netopt="--net=$interface --netfilter"
     fi
 
@@ -40,13 +44,14 @@ app_opts() {
     esac
 }
 
-# Get the name first connected device from NetworkManager.
-interface=`nmcli d | grep -m 1 connected | cut -d ' ' -f 1`
-
-while getopts "i:dh" opt; do
+while getopts "i:dnh" opt; do
     case $opt in
         i)
             interface=$OPTARG
+            network=true
+            ;;
+        n)
+            network=true
             ;;
         d)
             dev=true
